69a98124cb7db6be7ab58320a8c959227d9f44c8
Nelson Álvarez <nelsonalvarez_0118@outlook.es>
2026-01-14 12:36:56 -0300

    debug http

--- src/api/http.js
+++ src/api/http.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_BASE_URL;
-
-console.log("VITE_API_BASE_URL =", baseURL);
-
-export const http = axios.create({ baseURL });
+export const http = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  headers: {
+    "ngrok-skip-browser-warning": "true",
+  },
+});
